65421d9a344f2bb9d4581e927cfa388fc0a911f4
Andres Felipe Pardo Mesa <a.pardo6162@gmail.com>
2019-03-14 18:28:24 -0500

    EC contract added

---
+++ EC.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.4.25;
+/** 
+    Contrato usado por los nodos sentinelas para publicar los eventos de seguridad encontrados
+**/
+
+contract EC {
+    
+    event newEventCreated(uint256 timestamp,string _data);
+    
+    //mapa con los eventos almacenados por dirección del nodo
+    mapping(address => mapping(uint256 => Event))public  events;
+    //mapa on la cantidad de eventos publicados por nodo 
+    mapping(address => uint256)public  identifiers;
+    //cantidad de eventos permitidos por nodo
+    uint256 public threshold;
+    //estructura de los eventos que son almacenados dentro de los smart contracts 
+    struct Event {
+        //timestamp cuando se genera el evento 
+        uint256 timestamp;
+        //descripción del evento de seguridad
+        string data;
+    }
+    //constructor donde se define el limite de eventos que se pueden almancenar
+    constructor(uint256 _threshold) public { 
+        threshold=_threshold;
+    }
+
+    // Create a new Event.
+    function createEvent(uint256 timestamp,string _data) public {
+        uint256 id_count;
+        id_count=identifiers[msg.sender]+=1;
+        //si el limite es alcanzado se sobreescribre sobre el mapa
+        if(id_count==threshold)
+            id_count=1;
+        events[msg.sender][id_count]=Event(timestamp,_data);
+        emit newEventCreated(timestamp,_data);
+    }
+    
+    function setThreshold(uint256 _threshold){
+        threshold=_threshold;
+    }
+
+}
+
